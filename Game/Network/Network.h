29499f855705247748f13b4f8c0181e131b348bb
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2019-03-22 14:21:16 +0900

    リロード中に武器変更するとHUDと装備がずれる不具合の修正

--- Game/Network/Network.h
+++ Game/Network/Network.h
@@ -5,7 +5,7 @@
 #include "NetGameEventReceiver.h"
 #include "NetworkConst.h"
 
-static const wchar_t NETWORK_VER[] = L"Alpha17";
+static const wchar_t NETWORK_VER[] = L"Alpha18";
 
 class NetWorkManager : public IGameObject{
 public:
